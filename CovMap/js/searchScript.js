dd23b8e95cc219741722cc7f340f6d7c45efb9c2
HOME\Admin <lthkim01@gmail.com>
2021-09-05 21:51:06 +0700

    fix responsive + menu button + validation report form

--- CovMap/js/searchScript.js
+++ CovMap/js/searchScript.js
@@ -1,73 +1,79 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoidGhxdWFuZyIsImEiOiJja3NlaG11bm4xMHJhMm9wM3kzeDk3eWFyIn0.UaKcxdXdZmPIUa0PfbBqZw';
-  function openNav() {
+var isOpen = false;
+function openNav() {
     document.getElementById("mySidebar").style.width = "80px";
     document.getElementById("main").style.marginLeft = "80px";
 }
-
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
 
-function openCity(evt, cityName) {
-
-    var i, tabcontent, tablinks;
-
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+function clickNav() {
+    if(isOpen === false){
+        openNav();
+        isOpen = true;
     }
-
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    else{
+        closeNav()
+        isOpen = false;
     }
-
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-    // $(".tabcontent").show();
 }
-
-function closeTab() {
-    $(".tabcontent").hide();
-}
-
 navigator.geolocation.getCurrentPosition(sucessLocation, errorLocation)
 
-function sucessLocation(position){
+function sucessLocation(position) {
     setupMap([position.coords.longitude, position.coords.latitude])
 }
 
-function errorLocation(){
+function errorLocation() {
     setupMap([106.682133, 10.765055])
 };
 
-function setupMap(center){
+function setupMap(center) {
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: center,
         zoom: 15
-      });
+    });
     const nav = new mapboxgl.NavigationControl({
         visualizePitch: true
     });
-    map.addControl(nav, 'top-right');
     var directions = new MapboxDirections({
         accessToken: 'pk.eyJ1IjoidGhxdWFuZyIsImEiOiJja3NlaG11bm4xMHJhMm9wM3kzeDk3eWFyIn0.UaKcxdXdZmPIUa0PfbBqZw',
         unit: 'metric',
         language: 'fr-FR',
-      });
+    });
+    map.addControl(directions, 'top-right');
+    map.addControl(nav, 'top-right');
     console.log("done");
-    map.addControl(directions,'top-right');
+    
 }
+(function () {
+    'use strict'
 
-// function search(err){
-//     // console.log(err)
-//     var directions = new MapboxDirections({
-//         accessToken: 'pk.eyJ1IjoidGhxdWFuZyIsImEiOiJja3NlaG11bm4xMHJhMm9wM3kzeDk3eWFyIn0.UaKcxdXdZmPIUa0PfbBqZw',
-//         unit: 'metric',
-//       });
-//     map.addControl(directions,'bottom-right');
-// }
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+$('.form-check').change(function () {
+    var checked = $('#Reason4').is(':checked');
+    if (checked == 1) {
+        $("#other-reason").removeAttr('disabled');
+    }
+    else
+        $("#other-reason").attr('disabled', 'disabled');
+});
